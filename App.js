0b6b9d99760dc7344065a6ffb95393e3259ffccc
prathit <prathit@umich.edu>
2020-01-03 12:41:13 -0500

    App.js fix

--- App.js
+++ App.js
@@ -20,25 +20,13 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import 'react-native-gesture-handler';
-import FetchLocation from './components/FetchLocation';
+
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
-import UsersMap from './components/UsersMap';
 import SettingsScreen from './components/SettingsScreen';
 import MapScreen from './components/MapScreen';
 import FrontScreen from './components/FrontScreen'
-import Geolocation from '@react-native-community/geolocation';
-
-
-
-
-//export let userName = "defaultPranav";
-let timesNameChanged = 0;
-
-//const App: () => React$Node = () => {
-
-
 
 
 const MainNavigator = createStackNavigator({
